7572e5f5c2594ea35bfc830fdaafd77566468970
Edgard Kozlowski <edgardfernandes11@gmail.com>
2022-02-11 17:56:54 -0300

    Remove grid if

--- index.js
+++ index.js
@@ -16,72 +16,65 @@ document.addEventListener("DOMContentLoaded", function(event) {
   playBtn.src = 'https://res.cloudinary.com/dyw3e3f2c/image/upload/v1544757407/saveeit/play.svg'
   fullScreenBtn.src = 'https://res.cloudinary.com/dyw3e3f2c/image/upload/v1544757407/saveeit/fullscreen.svg'
 
-  // Get grid element
-  var isGridPage = document.querySelector('.grid')
-  
-  if (isGridPage !== null) {
-
-    // Play scroll
-    function pageScroll() {
-      if (isPlaying) {
-        playBtn.src = 'https://res.cloudinary.com/dyw3e3f2c/image/upload/v1544757407/saveeit/pause.svg'
-        window.scrollBy(0,1)
-        scrolldelay = setTimeout(pageScroll, 30)
-      } else {
-        playBtn.src = 'https://res.cloudinary.com/dyw3e3f2c/image/upload/v1544757407/saveeit/play.svg'
-      }
+  // Play scroll
+  function pageScroll() {
+    if (isPlaying) {
+      playBtn.src = 'https://res.cloudinary.com/dyw3e3f2c/image/upload/v1544757407/saveeit/pause.svg'
+      window.scrollBy(0,1)
+      scrolldelay = setTimeout(pageScroll, 30)
+    } else {
+      playBtn.src = 'https://res.cloudinary.com/dyw3e3f2c/image/upload/v1544757407/saveeit/play.svg'
     }
-
-    function toggleScroll () {
-      isPlaying = !isPlaying
-      pageScroll()
-    }
-
-    /* Fullscreen */
-    function toggleFullscreen () {
-      isFullScreen = !isFullScreen
-      fullScreenFunc()
-    }
-
-    function fullScreenFunc() {
-      if (isFullScreen === true) {
-        fullScreenBtn.src = 'https://res.cloudinary.com/dyw3e3f2c/image/upload/v1544757407/saveeit/close.svg'
-        document.documentElement.webkitRequestFullscreen()
-      } else {
-        fullScreenBtn.src = 'https://res.cloudinary.com/dyw3e3f2c/image/upload/v1544757407/saveeit/fullscreen.svg'
-        document.webkitExitFullscreen()
-      }
-    }
-
-    // Change fullscreen icon when close fullscreen with esc
-    document.addEventListener('webkitfullscreenchange', exitHandler)
-    function exitHandler() {
-        if (!document.webkitIsFullScreen) {
-            fullScreenBtn.src = 'https://res.cloudinary.com/dyw3e3f2c/image/upload/v1544757407/saveeit/fullscreen.svg'
-        }
-    }  
-
-    setTimeout(() => {
-
-      // Set buttons styles
-      playFullScreenContent.setAttribute(
-        'style',
-        'position: fixed; width: 100px; height: 50px; bottom: 25px; right: 25px; background-color: #000; color: #FFF; z-index: 9999; display: flex; justify-content: center;'
-      )
-      playPause.setAttribute('style', 'padding: 0;')
-      fullScreen.setAttribute('style', 'padding: 0;')
-
-      // Add elements on DOM
-      playPause.appendChild(playBtn)
-      fullScreen.appendChild(fullScreenBtn)
-      playFullScreenContent.appendChild(playPause)
-      playFullScreenContent.appendChild(fullScreen)
-
-      // Event
-      playPause.addEventListener('click', toggleScroll)
-      fullScreen.addEventListener('click', toggleFullscreen)
-
-    }, 3000)
-
   }
+
+  function toggleScroll () {
+    isPlaying = !isPlaying
+    pageScroll()
+  }
+
+  /* Fullscreen */
+  function toggleFullscreen () {
+    isFullScreen = !isFullScreen
+    fullScreenFunc()
+  }
+
+  function fullScreenFunc() {
+    if (isFullScreen === true) {
+      fullScreenBtn.src = 'https://res.cloudinary.com/dyw3e3f2c/image/upload/v1544757407/saveeit/close.svg'
+      document.documentElement.webkitRequestFullscreen()
+    } else {
+      fullScreenBtn.src = 'https://res.cloudinary.com/dyw3e3f2c/image/upload/v1544757407/saveeit/fullscreen.svg'
+      document.webkitExitFullscreen()
+    }
+  }
+
+  // Change fullscreen icon when close fullscreen with esc
+  document.addEventListener('webkitfullscreenchange', exitHandler)
+  function exitHandler() {
+      if (!document.webkitIsFullScreen) {
+          fullScreenBtn.src = 'https://res.cloudinary.com/dyw3e3f2c/image/upload/v1544757407/saveeit/fullscreen.svg'
+      }
+  }  
+
+  setTimeout(() => {
+
+    // Set buttons styles
+    playFullScreenContent.setAttribute(
+      'style',
+      'position: fixed; width: 100px; height: 50px; bottom: 25px; right: 25px; background-color: #000; color: #FFF; z-index: 9999; display: flex; justify-content: center;'
+    )
+    playPause.setAttribute('style', 'padding: 0;')
+    fullScreen.setAttribute('style', 'padding: 0;')
+
+    // Add elements on DOM
+    playPause.appendChild(playBtn)
+    fullScreen.appendChild(fullScreenBtn)
+    playFullScreenContent.appendChild(playPause)
+    playFullScreenContent.appendChild(fullScreen)
+
+    // Event
+    playPause.addEventListener('click', toggleScroll)
+    fullScreen.addEventListener('click', toggleFullscreen)
+
+  }, 3000)
 })
